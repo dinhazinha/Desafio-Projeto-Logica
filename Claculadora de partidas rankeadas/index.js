7a171ea8b7bf6a4ae3b2765f8ac860a6ef81a1f3
Ildecilda Fatima dos Santos <dinha.adm@gmail.com>
2023-11-01 10:29:02 -0300

    Melhorando ...

---
+++ Claculadora de partidas rankeadas/index.js
@@ -0,0 +1,17 @@
+var herois = [Homen-aranha, Hulk, Homen-de-Ferro, Doutor-Estranho, Pantera-Negra, Thor]
+
+let vitorias = ;
+let derrotas = ;
+
+saldoDeRankeadas = vitorias - derrotas
+
+function saldoDeRankeadas(vitorias, derrotas) {
+    return saldoDeRankeadas
+}
+
+console.log(saldoDeRankeadas)
+
+for(let i = 0; i < 10; i++)
+
+
+console.log("O Herói tem de saldo de " ${saldoVitorias}, "está no nível de" ${nivel})
